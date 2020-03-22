83a41d5cc4eb059adb6d5e2639df16b35c1d39c0
Alvaro Neira <alvaroneirareyes@gmail.com>
2020-03-21 22:20:23 -0300

    Correctness OK

--- src/com/alvaroneira/questions/MissingInteger.java
+++ src/com/alvaroneira/questions/MissingInteger.java
@@ -1,8 +1,8 @@
 package com.alvaroneira.questions;
 
 /**
- * Codility Demo Question
- *
+ * Codility Demo Question: Find the smallest positive integer that does not occur in a given sequence.
+ * 
  * Task description
  *
  * This is a demo task.
@@ -27,19 +27,112 @@ package com.alvaroneira.questions;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
+
+import com.alvaroneira.utils.ArrayUtils;
 import org.junit.Assert;
+
+import static com.alvaroneira.utils.ArrayUtils.contains;
+import static com.alvaroneira.utils.ArrayUtils.java8contains;
+
 public class MissingInteger {
-    public static int solution(int[] A) {
-        System.out.println("start");
-        return bruteForce(A);
+    public int solution(int[] A) {
+        return better4(A);
         // write your code in Java SE 8
     }
+
+    public static int better4(int[] A) {
+        if (A.length == 0) {
+            return 1;
+        }
+        Integer[] integers = IntStream.of(A).boxed().toArray(Integer[]::new);
+        Arrays.sort(integers);
+        if (integers[0] > 1) {
+            return 1;
+        }
+        Integer last = integers[0];
+        for (Integer integer : integers) {
+            if (integer < 1) {
+                continue;
+            }
+            if (last > 0) {
+                if (integer - last > 1) {
+                    return last + 1;
+                }
+            } else if (integer != 1) {
+                return 1;
+            }
+            last = integer;
+        }
+        return Math.max(1, last + 1);
+    }
+
+    public static int better3(int[] A){
+        if(A.length==0){
+            return 1;
+        }
+        Integer[] integers = IntStream.of( A ).boxed().toArray( Integer[]::new );
+        Arrays.sort(integers);
+        if(integers[0]>1){
+            return 1;
+        }
+        Integer last=integers[0];
+        for(Integer integer : integers){
+            if(integer<1){
+                continue;
+            }
+            if(integer-last>1){
+                return last>0 ? last+1:1;
+            }
+            last=integer;
+        }
+        return Math.max(1,last+1);
+    }
+
+    public static int better2(int[] A){
+        if(A.length==0){
+            return 1;
+        }
+        Integer[] integers = IntStream.of( A ).boxed().toArray( Integer[]::new );
+        Arrays.sort(integers);
+        if(integers[0]>1){
+            return 1;
+        }
+        Integer last=integers[0];
+        for(Integer i : integers){
+            if(i<1){
+                continue;
+            }
+            if(i-last>1){
+                return last+1;
+            }
+            last=i;
+        }
+        return Math.max(1,last+1);
+    }
+
+    public static int better1(int[] A){
+        if(A.length==0){
+            return 1;
+        }
+        Integer[] integers = IntStream.of( A ).boxed().toArray( Integer[]::new );
+        Arrays.sort(integers);
+        Integer last=integers[0];
+        for(Integer i : integers){
+            if(i<1){
+                continue;
+            }
+            if(i-last>1){
+                return last+1;
+            }
+            last=i;
+        }
+        return Math.max(1,last+1);
+    }
     public static int bruteForce(int[] A){
-        //List<Integer> aList = Arrays.asList(A);
         int i=1;
         for(;i<=1000000;){
-            System.out.println(i);
-            if(!contains(A,i)){
+            if(!java8contains(A,i)){
                 return i;
             }
             i++;
@@ -47,20 +140,34 @@ public class MissingInteger {
         return i;
     }
 
-    public static boolean contains(int[] A, int n){
-        for(int i=0;i<A.length;i++){
-            if(A[i]==n){
-                return true;
-            }
-        }
-        return false;
-    }
+
     public static void main(String[] args) {
-        Assert.assertEquals(solution(new int[]{1, 3, 6, 4, 1, 2}), 5);
-        Assert.assertEquals(solution(new int[]{1,2,3}), 4);
-        int[] A=new int[2];
+        MissingInteger mi=new MissingInteger();
+        int[] A;
+        Assert.assertEquals(mi.solution(new int[]{1, 3, 6, 4, 1, 2}), 5);
+        Assert.assertEquals(mi.solution(new int[]{1,2,3}), 4);
+        A=new int[2];
         A[0]=-1;A[1]=-2;
-        Assert.assertEquals(solution(A), 1);
+        Assert.assertEquals(mi.solution(A), 1);
+        Assert.assertEquals(1, mi.solution(new int[]{2}));
+        A=new int[2];
+        A[0]=-1000000;A[1]=1000000;
+        Assert.assertEquals(1, mi.solution(A));
+        Assert.assertEquals(6, mi.solution(new int[]{5,4,1,2,3}));
+        A=new int[2];
+        A[0]=-1000000;A[1]=1;
+        Assert.assertEquals(2, mi.solution(A));
+        A=new int[2];
+        A[0]=-1000000;A[1]=2;
+        Assert.assertEquals(1, mi.solution(A));
+        A=new int[6];
+        A[0]=-1000000;
+        A[1]=1;
+        A[2]=2;
+        A[3]=3;
+        A[4]=4;
+        A[5]=5;
+        Assert.assertEquals(6, mi.solution(A));
     }
 
 }
--- src/com/alvaroneira/utils/ArrayUtils.java
+++ src/com/alvaroneira/utils/ArrayUtils.java
@@ -1,5 +1,7 @@
 package com.alvaroneira.utils;
 
+import java.util.stream.IntStream;
+
 /**
  * Created by aneira on 3/18/17.
  */
@@ -10,5 +12,18 @@ public class ArrayUtils {
             retVal+=elem+",";
         }
         return retVal+"]";
+    }
+
+    public static boolean contains(int[] A, int n){
+        for(int i=0;i<A.length;i++){
+            if(A[i]==n){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean java8contains(int[] A, int n){
+        return IntStream.of(A).anyMatch(x -> x == n);
     }
 }
